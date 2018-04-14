87a93205d51e0b35a8565d85142242958f3f9c63
likeIronic <likeironic@163.com>
2018-04-14 14:46:02 +0800

    count_ones.py

---
+++ bit/count_ones.py
@@ -0,0 +1,32 @@
+"""
+
+Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+For example, the 32-bit integer '1' has binary representation
+00000000000000000000000000001011, so the function should return 3.
+T(n)- O(k) : k is the number of 1s present in binary representation.
+NOTE: this complexity is better than O(log n).
+e.g. for n = 00010100000000000000000000000000
+only 2 iterations are required.
+Number of loops is equal to the number of 1s in the binary representation.
+
+"""
+def count_ones_recur(n):
+    if not n:
+        return 0
+    else:
+        return 1 + count_ones_recur(n & (n-1))
+
+def count_ones_iter(n):
+    count = 0
+    while n:
+        n &= (n-1)
+        count += 1
+    return count
+
+
+if __name__=='__main__':
+    #n = 32
+    n = 31
+    print count_ones_recur(n)
+    print count_ones_iter(n)
+
